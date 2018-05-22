d425eddcaf5f3ebe8b9c21733a15f9c90da1d427
Wayne McDonald <wmcdonald@mpi.mb.ca>
2018-05-22 18:26:03 -0500

    created structure for player and enemy class. No console errors

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 /*--------------- Enemies --------------- */
 
-var Enemy = function() {
+let Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+	this.x = x;
+	this.y = y;
 };
 
 // Update the enemy's position, required method for game
@@ -27,15 +28,30 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var player = function(){
+var Player = function() {
 	this.sprite = 'images/char-boy.png';
 }
+
+player = new Player();
+
+Player.prototype.update = function() {
+	
+};
+
+// Draw the enemy on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function() {
+	
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+let allEnemies = [];
 
 
 // This listens for key presses and sends the keys to your
