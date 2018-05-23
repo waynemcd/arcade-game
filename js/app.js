6402766da5326cdfbb35732334b44727832c1890
Wayne McDonald <wmcdonald@mpi.mb.ca>
2018-05-22 21:52:14 -0500

    All assets display on screen

--- js/app.js
+++ js/app.js
@@ -1,21 +1,29 @@
 /*--------------- Enemies --------------- */
 
-let Enemy = function() {
+
+var speed;//enemy speed
+
+var Enemy = function(x,y,speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
     // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    // a helper we've provided to easily load image
 	this.x = x;
 	this.y = y;
+	this.speed = 100;
+	this.sprite = 'images/enemy-bug.png';
+	this.width = 101;
+	this.height = 171;
 };
 
+var enemy = new Enemy();
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+	this.x = this.x + this.speed * dt;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,6 +32,7 @@ Enemy.prototype.render = function() {
 };
 
 /*--------------- Player --------------- */
+
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -54,8 +63,11 @@ Player.prototype.handleInput = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-let allEnemies = [];
-
+var enemy1 = new Enemy(0,58,10);
+var enemy2 = new Enemy(0,143,20);
+var enemy2 = new Enemy(0,143,20);
+var enemy3 = new Enemy(0,224,30);
+var allEnemies = [enemy1, enemy2, enemy3];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
