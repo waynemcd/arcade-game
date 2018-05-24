c6708ee505972cdfb9f2c330862d3dd92396129f
Wayne McDonald <wmcdonald@mpi.mb.ca>
2018-05-23 20:23:37 -0500

    Enemy restarts x position once clears screen

--- js/app.js
+++ js/app.js
@@ -19,11 +19,23 @@ var Enemy = function(x,y,speed) {
 var enemy = new Enemy();
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+var enemy1 = new Enemy(0,58,10);
+var enemy2 = new Enemy(0,143,20);
+var enemy2 = new Enemy(0,143,20);
+var enemy3 = new Enemy(0,224,30);
+var allEnemies = [enemy1, enemy2, enemy3];
+
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    // Multiply movement by the dt parameter
+    // to ensure the game runs at the same speed on all computers.
 	this.x = this.x + this.speed * dt;
+		if (this.x > 521){ // Once the enemy reaches the end of the screen, 
+			this.x = 0;	   // restart at initial position
+		}
 };
 
 // Draw the enemy on the screen, required method for game
@@ -34,9 +46,7 @@ Enemy.prototype.render = function() {
 /*--------------- Player --------------- */
 
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// player class
 var Player = function() {
 	this.sprite = 'images/char-boy.png';
 	//Initial player starting position
@@ -50,35 +60,26 @@ Player.prototype.update = function() {
 	
 };
 
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+//Player movement
 Player.prototype.handleInput = function(allowedKeys) {
-	if(allowedKeys == 'left' && player.x > 0 ) { 
+	if(allowedKeys == 'left' && player.x > 0 ) { 			   // Determines how far left on screen player can travel
 		this.x -= 20; 
-		} else if(allowedKeys == 'right' && player.x < 420) { 
+		} else if(allowedKeys == 'right' && player.x < 420) {  // Determines how far right on screen player can travel
 		this.x += 20; 
-		} else if (allowedKeys == 'up' && player.y > 0) {
+		} else if (allowedKeys == 'up' && player.y > 0) {      // Determines how far player can travel to the top
 		this.y+= -20; 	
-		} else if (allowedKeys == 'down' && player.y < 400) {
+		} else if (allowedKeys == 'down' && player.y < 400) {  // Determines how far to the bottom player can go
 		this.y+= 20; 	
 	}
 };
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-var enemy1 = new Enemy(0,58,10);
-var enemy2 = new Enemy(0,143,20);
-var enemy2 = new Enemy(0,143,20);
-var enemy3 = new Enemy(0,224,30);
-var allEnemies = [enemy1, enemy2, enemy3];
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// This listens for key presses and sends the keys to Player.handleInput() method.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
