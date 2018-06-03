d86eb2bff28b306cdce8f5d432a2248534f51196
waynemcd <38333545+waynemcd@users.noreply.github.com>
2018-06-02 21:42:06 -0500

    Review updates completed

--- README.md
+++ README.md
@@ -1,14 +1,6 @@
-# Frontend-nanodegree-arcade-game
+frontend-nanodegree-arcade-game
+===============================
 
-![alt text](https://github.com/waynemcd/arcade-game/blob/master/arcade-game-screenshot.jpg "Game preview")
+**readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
 
-## About the game
-Classic Frogger style arcade game created using Object Oriented JavaScript as part of the Front End Developer Nanodegree program.
 
-## How to launch the Game
-Download the folder from https://github.com/waynemcd/arcade-game/archive/master.zip and open index.html in your browser.
-Alternitively, The game can be previewed here:
-http://htmlpreview.github.io/?https://github.com/waynemcd/arcade-game/blob/master/index.html
-
- ## Instructions
- Use the arrow keys to navigate the player safely to the water while avoiding ladybugs! Score points each time the water is reached.
--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@
 
 
 var speed;//enemy speed
-var score = 0;
 var displayScore = document.getElementById('playerScore');
 
 var Enemy = function(x,y,speed) {
@@ -12,7 +11,7 @@ var Enemy = function(x,y,speed) {
     // a helper we've provided to easily load image
 	this.x = x;
 	this.y = y;
-	this.speed = 90;
+	this.speed = 50;
 	this.sprite = 'images/enemy-bug.png';
 	this.width = 101;
 	this.height = 171;
@@ -28,9 +27,10 @@ var enemy2 = new Enemy(-900,224,10);
 var enemy3 = new Enemy(-0,300,90);
 var enemy4 = new Enemy(-230,143,20);
 var enemy5 = new Enemy(-190,224,10);
-var enemy6 = new Enemy(-770,300,90);
+//var enemy6 = new Enemy(-770,300,90);
 //var enemy7 = new Enemy(-900,300,90);
-var allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5, enemy6];
+
+var allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5];
 
 Enemy.prototype.update = function(dt) {
     // Multiply movement by the dt parameter
@@ -46,30 +46,14 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
 /*--------------- Player --------------- */
 
 
 // player class
 var Player = function() {
 	this.sprite = 'images/char-boy.png';
-	//Initial player starting position
-	this.reset = function() {
-		this.x = 220;
-		this.y = 380;
-	};
-	this.score = function() {
-		score++;  // Increment score
-		displayScore.innerHTML = score; //Update score onscreen
-	};
-	this.checkCollision = function() { // Check if player touches enemy if so reset position
-		for(var c = 0; c < allEnemies.length;c++){
-			if (player.x < allEnemies[c].x + 98  && player.x + 69  > allEnemies[c].x &&
-				player.y < allEnemies[c].y + 76 && player.y + 20 > allEnemies[c].y) {
-			// Collision detected, reset player to starting position
-			player.reset();
-			}	
-		}
-	}
+	this.score = 0;
 };
 
 player = new Player();
@@ -77,7 +61,28 @@ player = new Player();
 Player.prototype.update = function() {
 	this.checkCollision(); //Check if an interaction took place
 };
-//
+
+Player.prototype.scored = function() {
+	this.score++;  // Increment score
+	displayScore.innerHTML = this.score; //Update score onscreen
+};
+
+Player.prototype.reset = function() { //Initial player starting position
+	this.x = 220;
+	this.y = 380;
+}
+
+Player.prototype.checkCollision = function() { // Check if player touches enemy if so reset position
+	for(var c = 0; c < allEnemies.length;c++) {
+			if (this.x < allEnemies[c].x + 98  && this.x + 69  > allEnemies[c].x &&
+				this.y < allEnemies[c].y + 76 && this.y + 20 > allEnemies[c].y) {
+			// Collision detected, reset player to starting position
+			this.reset();
+		}	
+	}
+};
+
+
 // Draw the enemy on the screen
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -85,19 +90,19 @@ Player.prototype.render = function() {
 
 //Player movement
 Player.prototype.handleInput = function(allowedKeys) {
-	if(allowedKeys == 'left' && player.x > 0 ) { 			   // Determines how far left on screen player can travel
-		this.x -= 20; 
+	if(allowedKeys == 'left' && player.x > 20 ) { 			   // Determines how far left on screen player can travel
+		this.x -= 40; 
 		} else if(allowedKeys == 'right' && player.x < 420) {  // Determines how far right on screen player can travel
-		this.x += 20; 
+		this.x += 40; 
 		} else if (allowedKeys == 'up' && player.y > 40) {      // Determines how far player can travel to the top
-		this.y+= -20;
+		this.y+= -40;
 		} else if (allowedKeys == 'down' && player.y < 450) {  // Determines how far to the bottom player can go
-		this.y+= 20; 
+		this.y+= 40; 
 		}
 		// If player reaches the water reset to starting position
-		if(player.y === 40){
-			player.reset();
-			player.score(); //Increment score
+		if(this.y === 60){
+			this.reset();
+			this.scored(); //Increment score
 		}
 };
 
